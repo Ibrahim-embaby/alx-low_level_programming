d387516b454dcc3cb4b77d01944ddf063f303b68
ibrahim <ibrahimembaby10@gmail.com>
2023-02-18 00:47:56 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - a program to print the alphabet except two letters
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a == 'e' || a == 'q')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(a);
+		}
+	}
+	return (0);
+}
+
