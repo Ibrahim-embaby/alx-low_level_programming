93fffd25b4b253e24b30d2adf1339ba77571d1bf
ibrahim <ibrahimembaby10@gmail.com>
2023-02-23 23:36:57 -0800

    test

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_triangle - a function that prints a triangle, followed by a new line
+ * Return: void
+ */
+void print_triangle(int size)
+{
+
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * Return: void
+ */
+void print_line(int n)
+{
+
+}
