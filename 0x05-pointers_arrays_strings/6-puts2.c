fc5329a78b714be8a73c82473250c8b7fa4c2223
ibrahim <ibrahimembaby10@gmail.com>
2023-02-27 12:13:25 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,17 @@
 #include "main.h"
 /**
+ * puts2 - a function that prints every other character of a string
+ * @str: string
  */
 void puts2(char *str)
 {
-	char *s = str;
-	*s = 'e';
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		if (*(str + 1) == '\0')
+		{
+			return;
+		}
+		str = str + 2;
+	}
 }
