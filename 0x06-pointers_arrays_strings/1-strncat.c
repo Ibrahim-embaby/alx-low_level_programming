4aeb72063553f12dd0c290c772b090b8f73863da
ibrahim <ibrahimembaby10@gmail.com>
2023-03-01 01:49:29 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: the dest string
+ * @src: the src string
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
